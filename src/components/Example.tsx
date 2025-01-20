53a89a7e3e9e208d17b8ad6b54c9ebe10d6b1677
Phongsakorn0 <pond.phongsakorn1654@gmail.com>
2025-01-20 21:10:15 +0700

    Initial Project 🚀

---
+++ src/components/Example.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+
+export const Example: React.FC = () => {
+  return (
+    <>
+      <div>
+        <h1>Example</h1>
+        <p>This is an example component. It is rendered on the client side.</p>
+      </div>
+    </>
+  );
+};
