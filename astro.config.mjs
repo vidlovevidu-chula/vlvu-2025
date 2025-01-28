af8c8a0486919e57b6eee9646ea3bfa52c8e6a33
Mintada P. <mintada.phuangminthada@gmail.com>
2025-01-29 01:12:30 +0700

    feat: install shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,10 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), react()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
 });
