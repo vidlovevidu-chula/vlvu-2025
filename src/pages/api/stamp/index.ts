16c76ec1ed8f7cc542d4d7f6f32188336cf549f1
Phongsakorn0 <pond.phongsakorn1654@gmail.com>
2025-02-02 01:16:26 +0700

    build : integrate vercel

--- src/pages/api/stamp/index.ts
+++ src/pages/api/stamp/index.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from "astro";
 import { getFirestore, FieldValue } from "firebase-admin/firestore";
-import { app } from "../../firebase/server.ts";
+import { app } from "../../../firebase/server.ts";
 
 const db = getFirestore(app);
 
