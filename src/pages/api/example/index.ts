5540a1d705abb0a8fc4a4e80f1d08f4e06068ff1
Phongsakorn0 <pond.phongsakorn1654@gmail.com>
2025-02-02 01:12:26 +0700

    build : integrate vercel

--- src/pages/api/example/index.ts
+++ src/pages/api/example/index.ts
@@ -1,14 +1,10 @@
 import type { APIRoute } from "astro";
-import { app } from "../../../firebase/server";
-import { getFirestore } from "firebase-admin/firestore";
 
-const db = getFirestore(app);
-const example = db.collection("wai");
 
 export const GET: APIRoute = async () => {
-  const snapshot = await example.get();
-  const data = snapshot.docs.map((doc) => doc.data());
-  return new Response(JSON.stringify(data), {
+  return new Response(JSON.stringify({
+    "test":"หมอลำ 🧕🧕🧕🧕🧕🧕🧕🧕"
+  }), {
     status: 200,
     headers: {
       "content-type": "application/json",
