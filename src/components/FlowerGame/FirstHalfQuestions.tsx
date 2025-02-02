ea8f717f805af385fd89ee751822bc588915643f
sasirasys <131147291+Sasirasys@users.noreply.github.com>
2025-02-03 00:38:53 +0700

    try add algo to scenes (not done)

--- src/components/FlowerGame/FirstHalfQuestions.tsx
+++ src/components/FlowerGame/FirstHalfQuestions.tsx
@@ -11,11 +11,26 @@ export const FirstHalfQuestions: React.FC<Props> = ({
 }) => {
   return (
     <>
-      <h1>Question {questionNumber}</h1>
-      <div className="flex gap-x-2">
-        <button onClick={() => onFirstHalfAnswer("charity")}>Charity</button>
-        <button onClick={() => onFirstHalfAnswer("sensory")}>Sensory</button>
-        <button onClick={() => onFirstHalfAnswer("harmony")}>Harmony</button>
+      <h1 className="text-red-500">Question {questionNumber}</h1>
+      <div className="flex flex-col gap-x-2 gap-y-10 text-red-500 max-[450px]:gap-y-[8vw] max-[450px]:text-[4vw]">
+        <button
+          className="border border-red-500 max-[450px]:h-[7wv]"
+          onClick={() => onFirstHalfAnswer("charity")}
+        >
+          charity
+        </button>
+        <button
+          className="border border-red-500"
+          onClick={() => onFirstHalfAnswer("sensory")}
+        >
+          sensory
+        </button>
+        <button
+          className="border border-red-500"
+          onClick={() => onFirstHalfAnswer("harmony")}
+        >
+          harmony
+        </button>
       </div>
     </>
   );
