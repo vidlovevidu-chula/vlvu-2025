3969c663c653560d25203918897adf94a80c9e80
Phongsakorn0 <pond.phongsakorn1654@gmail.com>
2025-02-06 22:07:36 +0700

    fix(env): env name

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,21 +1,21 @@
 interface ImportMetaEnv {
-  readonly FIREBASE_PRIVATE_KEY_ID: string;
-  readonly FIREBASE_PRIVATE_KEY: string;
-  readonly PUBLIC_FIREBASE_PROJECT_ID: string;
-  readonly FIREBASE_CLIENT_EMAIL: string;
-  readonly FIREBASE_CLIENT_ID: string;
-  readonly FIREBASE_AUTH_URI: string;
-  readonly FIREBASE_TOKEN_URI: string;
-  readonly FIREBASE_AUTH_CERT_URL: string;
-  readonly FIREBASE_CLIENT_CERT_URL: string;
-  readonly API_KEY: string;
-  readonly AUTH_DOMAIN: string;
-  readonly DATABASE_URL: string;
-  readonly PUBLIC_PROJECT_ID: string;
-  readonly STORAGE_BUCKET: string;
-  readonly MESSAGING_SENDER_ID: string;
-  readonly APP_ID: string;
-  readonly MEASUREMENT_ID: string;
+  readonly VITE_FIREBASE_PRIVATE_KEY_ID: string;
+  readonly VITE_FIREBASE_PRIVATE_KEY: string;
+  readonly VITE_PUBLIC_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_CLIENT_EMAIL: string;
+  readonly VITE_FIREBASE_CLIENT_ID: string;
+  readonly VITE_FIREBASE_AUTH_URI: string;
+  readonly VITE_FIREBASE_TOKEN_URI: string;
+  readonly VITE_FIREBASE_AUTH_CERT_URL: string;
+  readonly VITE_FIREBASE_CLIENT_CERT_URL: string;
+  readonly VITE_API_KEY: string;
+  readonly VITE_AUTH_DOMAIN: string;
+  readonly VITE_DATABASE_URL: string;
+  readonly VITE_PROJECT_ID: string;
+  readonly VITE_STORAGE_BUCKET: string;
+  readonly VITE_MESSAGING_SENDER_ID: string;
+  readonly VITE_APP_ID: string;
+  readonly VITE_MEASUREMENT_ID: string;
 }
 
 interface ImportMeta {
