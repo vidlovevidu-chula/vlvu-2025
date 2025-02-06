4eeba62e460c832c328e39f2a9b7f29de41e7c96
Phongsakorn0 <pond.phongsakorn1654@gmail.com>
2025-02-06 22:15:25 +0700

    fix(env): env name

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,14 +8,6 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_TOKEN_URI: string;
   readonly VITE_FIREBASE_AUTH_CERT_URL: string;
   readonly VITE_FIREBASE_CLIENT_CERT_URL: string;
-  readonly VITE_API_KEY: string;
-  readonly VITE_AUTH_DOMAIN: string;
-  readonly VITE_DATABASE_URL: string;
-  readonly VITE_PROJECT_ID: string;
-  readonly VITE_STORAGE_BUCKET: string;
-  readonly VITE_MESSAGING_SENDER_ID: string;
-  readonly VITE_APP_ID: string;
-  readonly VITE_MEASUREMENT_ID: string;
 }
 
 interface ImportMeta {
