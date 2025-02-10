ca00146bbb1d962b12bae1cf530e0448890f0dc1
Mintada P. <mintada.phuangminthada@gmail.com>
2025-02-11 00:55:08 +0700

    fix/grass-bottom & navburger

--- src/components/home/Logo.tsx
+++ src/components/home/Logo.tsx
@@ -2,12 +2,10 @@ import * as React from "react";
 
 const Logo: React.FC = () => {
   return (
-    <div className="flex justify-between items-center px-4 pt-2">
-      <div className="flex items-center">
-        <img src="/images/home/vidva.webp" className="w-10" />
-        <img src="/images/home/vidya.webp" className="w-10" />
-      </div>
+    <div className="flex justify-center items-center gap-1 pt-2">
+      <img src="/images/home/vidva.webp" className="w-10" />
       <img src="/images/home/vlvu.webp" className="w-10" />
+      <img src="/images/home/vidya.webp" className="w-10 ml-2" />
     </div>
   );
 };
