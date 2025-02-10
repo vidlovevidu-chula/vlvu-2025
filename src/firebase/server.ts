7cdfee5e9a9ea9b56af320aa5b9a795c490a0a8d
Thames <chiratt.thames@gmail.com>
2025-02-11 00:28:54 +0700

    Update server.ts

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -4,7 +4,7 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 const activeApps = getApps();
 const serviceAccount = {
   type: "service_account",
-  project_id: import.meta.env.VITE_PROJECT_ID,
+  project_id: "vlvu2025",
   private_key_id: import.meta.env.VITE_FIREBASE_PRIVATE_KEY_ID,
   private_key: import.meta.env.VITE_FIREBASE_PRIVATE_KEY,
   client_email: import.meta.env.VITE_FIREBASE_CLIENT_EMAIL,
@@ -15,16 +15,9 @@ const serviceAccount = {
   client_x509_cert_url: import.meta.env.VITE_FIREBASE_CLIENT_CERT_URL,
 };
 
-const initApp = () => {
-  if (import.meta.env.PROD) {
-    console.info("PROD env detected. Using default service account.");
-    // Use default config in firebase functions. Should be already injected in the server by Firebase.
-    return initializeApp();
-  }
-  console.info("Loading service account from env.");
-  return initializeApp({
-    credential: cert(serviceAccount as ServiceAccount),
-  });
-};
-
-export const app = activeApps.length === 0 ? initApp() : activeApps[0];
+export const app =
+  activeApps.length === 0
+    ? initializeApp({
+        credential: cert(serviceAccount as ServiceAccount),
+      })
+    : activeApps[0];
